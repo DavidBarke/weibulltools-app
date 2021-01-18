5ab47ea70165a869b1a151d1abca3c08c10e3b47
David Barkemeyer <david.barkemeyer@gmx.de>
2021-01-18 23:04:05 +0100

    Move estimate_cdf in separate module

---
+++ modules/tab_probability_estimation/estimate_cdf.R
@@ -0,0 +1,74 @@
+estimate_cdf_ui <- function(id) {
+  ns <- shiny::NS(id)
+
+  bs4Dash::box(
+    width = NULL,
+    solidHeader = TRUE,
+    status = "primary",
+    title = "Non-Parametric Failure Probabilities",
+    htmltools::p(
+      "Functions for estimation and visualization of failure probabilities"
+    ),
+    r_function(
+      name = "estimate_cdf",
+      r_function_arg(
+        "x",
+
+      ),
+      r_function_arg(
+        "methods",
+        shiny::selectInput(
+          inputId = ns("select_methods"),
+          label = NULL,
+          choices = c("mr", "johnson", "kaplan", "nelson"),
+          width = "100%"
+        )
+      ),
+      r_function_arg(
+        "options",
+        shiny::uiOutput(
+          outputId = ns("options")
+        )
+      )
+    )
+  )
+}
+
+estimate_cdf_server <- function(id, .values) {
+  shiny::moduleServer(
+    id,
+    function(input, output, session) {
+
+      ns <- session$ns
+
+      output$options <- shiny::renderUI({
+        if (shiny::req(input$select_methods[[1]]) == "mr") {
+          htmltools::tagList(
+            r_function_arg(
+              "mr_method",
+              width = 6,
+              shiny::selectInput(
+                inputId = ns("mr_method"),
+                label = NULL,
+                choices = c("benard", "invbeta"),
+                width = "100%"
+              )
+            ),
+            r_function_arg(
+              "mr_ties.method",
+              width = 6,
+              shiny::selectInput(
+                inputId = ns("mr_ties.method"),
+                label = NULL,
+                choices = c("max", "min", "average"),
+                width = "100%"
+              )
+            )
+          )
+        } else {
+          "list()"
+        }
+      })
+    }
+  )
+}
--- modules/tab_probability_estimation/probability_estimation.R
+++ modules/tab_probability_estimation/probability_estimation.R
@@ -4,36 +4,8 @@ probability_estimation_ui <- function(id) {
   shiny::fluidRow(
     shiny::column(
       width = 6,
-      bs4Dash::box(
-        width = NULL,
-        solidHeader = TRUE,
-        status = "primary",
-        title = "Non-Parametric Failure Probabilities",
-        htmltools::p(
-          "Functions for estimation and visualization of failure probabilities"
-        ),
-        r_function(
-          name = "estimate_cdf",
-          r_function_arg(
-            "x",
-
-          ),
-          r_function_arg(
-            "methods",
-            shiny::selectInput(
-              inputId = ns("select_methods"),
-              label = NULL,
-              choices = c("mr", "johnson", "kaplan", "nelson"),
-              width = "100%"
-            )
-          ),
-          r_function_arg(
-            "options",
-            shiny::uiOutput(
-              outputId = ns("options")
-            )
-          )
-        )
+      estimate_cdf_ui(
+        id = ns("estimate_cdf")
       )
     )
   )
@@ -46,34 +18,10 @@ probability_estimation_server <- function(id, .values) {
 
       ns <- session$ns
 
-      output$options <- shiny::renderUI({
-        if (shiny::req(input$select_methods[[1]]) == "mr") {
-          htmltools::tagList(
-            r_function_arg(
-              "mr_method",
-              width = 6,
-              shiny::selectInput(
-                inputId = ns("mr_method"),
-                label = NULL,
-                choices = c("benard", "invbeta"),
-                width = "100%"
-              )
-            ),
-            r_function_arg(
-              "mr_ties.method",
-              width = 6,
-              shiny::selectInput(
-                inputId = ns("mr_ties.method"),
-                label = NULL,
-                choices = c("max", "min", "average"),
-                width = "100%"
-              )
-            )
-          )
-        } else {
-          "list()"
-        }
-      })
+      estimate_cdf_server(
+        id = "estimate_cdf",
+        .values = .values
+      )
     }
   )
 }
