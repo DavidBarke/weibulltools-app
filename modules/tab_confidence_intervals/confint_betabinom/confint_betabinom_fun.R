65e7bf5cf67d849ed896895f4b6179333d86e7f5
David Barkemeyer <david.barkemeyer@gmx.de>
2021-01-19 12:19:47 +0100

    Function UI for confint_xxx

--- modules/tab_confidence_intervals/confint_betabinom/confint_betabinom_fun.R
+++ modules/tab_confidence_intervals/confint_betabinom/confint_betabinom_fun.R
@@ -1,8 +1,29 @@
 confint_betabinom_fun_ui <- function(id) {
   ns <- shiny::NS(id)
 
-  htmltools::tagList(
-
+  bs4Dash::box(
+    width = NULL,
+    solidHeader = TRUE,
+    status = "primary",
+    title = "Beta Binomial Confidence Bounds",
+    r_function(
+      "confint_betabinom",
+      r_function_arg(
+        "x"
+      ),
+      r_b_lives_arg(
+        inputId = ns("b_lives")
+      ),
+      r_bounds_arg(
+        inputId = ns("bounds")
+      ),
+      r_conf_level_arg(
+        inputId = ns("conf_level")
+      ),
+      r_direction_arg(
+        inputId = ns("direction")
+      )
+    )
   )
 }
 
--- modules/ui/r_function_ui.R
+++ modules/ui/r_function_ui.R
@@ -35,7 +35,7 @@ r_function_arg <- function(name, ..., width = 3) {
 r_distribution_arg <- function(inputId, width = 3) {
   r_function_arg(
     name = "distribution",
-    shiny::selectInput(
+    preSelectInput(
       inputId = inputId,
       label = NULL,
       choices = c(
@@ -61,6 +61,72 @@ r_conf_level_arg <- function(inputId, width = 3) {
       width = "100%"
     ),
     width = width
+  )
+}
+
+r_b_lives_arg <- function(inputId, width = 3) {
+  choices <- c("0.01", "0.1", "0.5")
+
+  r_function_arg(
+    name = "b_lives",
+    preSelectInput(
+      inputId = inputId,
+      label = NULL,
+      choices = choices,
+      multiple = TRUE,
+      selected = choices,
+      width = "100%"
+    )
+  )
+}
+
+r_bounds_arg <- function(inputId, width = 3) {
+  choices <- c("two_sided", "lower", "upper")
+
+  r_function_arg(
+    name = "bounds",
+    preSelectInput(
+      inputId = inputId,
+      label = NULL,
+      choices = choices,
+      width = "100%"
+    )
+  )
+}
+
+r_direction_arg <- function(inputId, width = 3) {
+  r_function_arg(
+    name = "direction",
+    preSelectInput(
+      inputId = inputId,
+      label = NULL,
+      choices = c("y", "x"),
+      width = "100%"
+    )
+  )
+}
+
+preSelectInput <- function(inputId,
+                           label,
+                           choices,
+                           selected = NULL,
+                           multiple = FALSE,
+                           selectize = TRUE,
+                           width = "100%",
+                           size = NULL
+) {
+  htmltools::div(
+    class = "pre",
+    shiny::selectInput(
+      inputId = inputId,
+      label = label,
+      choices = choices,
+      selected = selected,
+      multiple = multiple,
+      selectize = selectize,
+      width = width,
+      size = size
+    )
   )
 }
 
