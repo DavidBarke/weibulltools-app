3f0a6a33cc12ce1a5247473b5976c2e2dba1d1aa
David Barkemeyer <david.barkemeyer@gmx.de>
2021-01-21 13:05:45 +0100

    Restructure probability_estimation files. Add helper function for converting xml to html. Connect code and result tabsetPanel with each other.

--- modules/tab_datasets/dataset.R
+++ modules/tab_datasets/dataset.R
@@ -5,28 +5,10 @@ dataset_ui <- function(id, ref_name) {
 
   contents <- xml2::xml_find_all(ref_html, "//div[contains(@class, 'contents')]")
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
   shiny::fluidRow(
     shiny::column(
       width = 6,
       xml2html(contents)
-=======
->>>>>>> 7db7450... Add datasets tab.
-  tmp <- tempfile(fileext = ".html")
-  xml2::write_html(contents, tmp)
-
-  contents <- htmltools::includeHTML(tmp)
-
-  shiny::fluidRow(
-    shiny::column(
-      width = 6,
-      contents
-<<<<<<< HEAD
-=======
->>>>>>> 238d00b... Add datasets tab.
->>>>>>> 7db7450... Add datasets tab.
     ),
     shiny::column(
       width = 6,
