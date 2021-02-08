5c9d73287419a85d7fc1f4bc49947980907c1fe3
David Barkemeyer <david.barkemeyer@gmx.de>
2021-02-08 13:25:16 +0100

    Improved error handling in list_result (#8). Set locale English

--- modules/generic_modules/list_result.R
+++ modules/generic_modules/list_result.R
@@ -1,9 +1,17 @@
 list_result_ui <- function(id) {
   ns <- shiny::NS(id)
 
-  shiny::uiOutput(
-    outputId = ns("list")
-  ) %>% shinycssloaders::withSpinner()
+  htmltools::tagList(
+    shiny::textOutput(
+      outputId = ns("error")
+    ),
+    shiny::textOutput(
+      outputId = ns("warning")
+    ),
+    shiny::uiOutput(
+      outputId = ns("list")
+    ) %>% shinycssloaders::withSpinner()
+  )
 }
 
 list_result_server <- function(id, .values, obj_r, dynamic = FALSE) {
@@ -15,21 +23,57 @@ list_result_server <- function(id, .values, obj_r, dynamic = FALSE) {
 
       server_env <- new.env()
 
+      error_message_rv <- shiny::reactiveVal(character())
+      warning_message_rv <- shiny::reactiveVal(character())
+
+      output$error <- shiny::renderText({
+        error_message_rv()
+      })
+
+      output$warning <- shiny::renderText({
+        paste(warning_message_rv(), collapse = "\n")
+      })
+
       names_r <- shiny::reactive({
+        env <- environment()
+        x <- character()
         tryCatch(
-          names(obj_r()),
-          error = function(e) character()
+          withCallingHandlers(
+            {
+              # Clean up error and warning messages
+              error_message_rv(character())
+              warning_message_rv(character())
+              # Assign names if no errors occured
+              env$x <- names(obj_r())
+            },
+            warning = function(e) {
+              warning_message_rv(
+                c(
+                  shiny::isolate(warning_message_rv()),
+                  e$message
+                )
+              )
+              invokeRestart("muffleWarning")
+            }
+          ),
+          error = function(e) {
+            error_message_rv(e$message)
+            character()
+          }
         )
+        x
       })
 
       names_rv <- shiny::reactiveVal(character())
 
       shiny::observeEvent(names_r(), {
+        # Set names of obj_r if modified
         if (!identical(names_r(), names_rv())) {
           names_rv(names_r())
         }
       }, ignoreInit = TRUE)
 
+      # Only update items when names change
       items_r <- shiny::eventReactive(names_rv(), {
         items <- purrr::map(names_r(), function(name) {
           output_name <- "item" %_% name
