fb7837f0c51499c20807bcc140b49ef3a2643dde
David Barkemeyer <david.barkemeyer@gmx.de>
2021-02-08 22:00:29 +0100

    Closes #41

--- modules/generic_modules/error_display.R
+++ modules/generic_modules/error_display.R
@@ -2,10 +2,10 @@ error_display_ui <- function(id) {
   ns <- shiny::NS(id)
 
   htmltools::tagList(
-    shiny::textOutput(
+    shiny::uiOutput(
       outputId = ns("error")
     ),
-    shiny::textOutput(
+    shiny::uiOutput(
       outputId = ns("warning")
     )
   )
@@ -18,18 +18,42 @@ error_display_server <- function(id, .values, obj_r) {
 
       ns <- session$ns
 
-      error_message_rv <- shiny::reactiveVal(NULL)
+      # Start with error, otherwise code that is dependent on the error_r return
+      # of this module fails (and so does the application) before tryCatch of
+      # this module was executed
+      error_message_rv <- shiny::reactiveVal("")
       warning_message_rv <- shiny::reactiveVal(character())
 
-      output$error <- shiny::renderText({
-        error_message_rv()
+      output$error <- shiny::renderUI({
+        if (!is.null(error_message_rv())) {
+          htmltools::tagList(
+            htmltools::h5("The following error was recorded:"),
+            htmltools::pre(error_message_rv())
+          )
+        }
       })
 
-      output$warning <- shiny::renderText({
-        paste(warning_message_rv(), collapse = "\n")
+      output$warning <- shiny::renderUI({
+        len <- length(warning_message_rv())
+        heading <- if (len == 1) {
+          htmltools::h5("The following warning was recorded:")
+        } else if (len > 1) {
+          htmltools::h5("The following warnings were recorded:")
+        }
+
+        warning_items <- purrr::map(warning_message_rv(), function(message) {
+          htmltools::tags$pre(message)
+        })
+
+        if (len) {
+          htmltools::tagList(
+            heading,
+            warning_items
+          )
+        }
       })
 
-      shiny::observeEvent(suppressWarnings(try(obj_r(), silent = TRUE)), {
+      shiny::observe({
         tryCatch(
           withCallingHandlers(
             {
@@ -53,7 +77,7 @@ error_display_server <- function(id, .values, obj_r) {
             error_message_rv(e$message)
           }
         )
-      }, priority = 1)
+      })
 
       return_list <- list(
         error_r = shiny::reactive(!is.null(error_message_rv()))
