b78a065e2c073c0aa3e3233698846c1276313a55
David Barkemeyer <david.barkemeyer@gmx.de>
2021-02-21 19:49:54 +0100

    Closes #19

--- init/reference.R
+++ init/reference.R
@@ -34,12 +34,52 @@ get_references <- function() {
 download_reference <- function(
                     dir,
                     references = get_references(),
-                    pkgdown = "https://tim-tu.github.io/weibulltools/reference/"
+                    pkgdown = "https://tim-tu.github.io/weibulltools/reference"
 ) {
   purrr::walk(paste0(references, ".html"), function(ref) {
     curl::curl_download(
-      url = paste0(pkgdown, ref),
+      url = paste0(pkgdown, "/", ref),
       destfile = file.path(dir, ref)
     )
   })
+}
+
+
+replace_links <- function(dir,
+                          pkgdown = "https://tim-tu.github.io/weibulltools"
+) {
+  ref_files <- list.files(dir, pattern = "\\.html$")
+  ref_paths <- file.path(dir, ref_files)
+
+  purrr::walk(ref_paths, function(path) {
+    ref_html <- xml2::read_html(path)
+
+    hrefs <- ref_html %>% xml2::xml_find_all("//a") %>% xml2::xml_attr("href")
+
+    hrefs <- purrr::map_chr(hrefs, function(href) {
+      if ( stringr::str_detect(href, "^\\.\\.*")) {
+        href <- stringr::str_replace(href, "^\\.\\.", pkgdown)
+      } else if (stringr::str_detect(href, "^[A-Za-z_]*\\.html")) {
+        href <- paste0(pkgdown, "/reference/", href)
+      }
+
+      href
+    })
+
+    a <- ref_html %>% xml2::xml_find_all("//a")
+    xml2::xml_attr(a, "href") <- hrefs
+
+    xml2::write_html(ref_html, path)
+  })
+}
+
+
+download_pkgdown <- function(dir,
+                            pkgdown = "https://tim-tu.github.io/weibulltools"
+) {
+  pkgdown_reference <- paste0(pkgdown, "/reference")
+
+  download_reference(dir, get_references(), pkgdown_reference)
+
+  replace_links(dir, pkgdown)
 }
