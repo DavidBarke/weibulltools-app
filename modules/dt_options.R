eb4a9803a0900ae312689bdd33e188b51f9490e2
David Barkemeyer <david.barkemeyer@gmx.de>
2021-03-25 11:02:23 +0100

    Format numeric columns (except "status") in all datatableOutputs

--- modules/dt_options.R
+++ modules/dt_options.R
@@ -8,6 +8,15 @@ dt_options <- function() {
   )
 }
 
-datatable_wrapper <- function(...) {
-  DT::datatable(..., class = "display compact")
+datatable_wrapper <- function(data, ...) {
+  is_numeric_column <- purrr::map_lgl(names(data), ~ {
+    !(. == "status") && is.numeric(data[[.]])
+  })
+
+  DT::datatable(data = data, ..., class = "display compact") %>%
+    DT::formatSignif(
+      # Add FALSE for row index column
+      columns = c(FALSE, is_numeric_column),
+      digits = 4
+    )
 }
