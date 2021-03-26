bf257bcce9e2b4653362f077edfc3bd3b76b15d0
David Barkemeyer <david.barkemeyer@gmx.de>
2021-03-26 09:18:01 +0100

    Getting Started: plot_mod for rank_regression and ml_estimation

--- modules/tab_getting_started/gs_plot_mod.R
+++ modules/tab_getting_started/gs_plot_mod.R
@@ -4,19 +4,35 @@ gs_plot_mod_ui <- function(id) {
   text_box(
     title = "Visualization of Parametric Lifetime Distributions",
     htmltools::includeMarkdown("getting_started/plot_mod.md"),
-    r_code(
-      'p_weib %>%',
-      '  plot_mod(',
-      '    rr',
-      '    title_trace = "Rank Regression"',
-      '  ) %>%',
-      '  plot_mod(',
-      '    mle,',
-      '    title_trace = "ML Estimation',
-      '  )'
-    ),
-    plotly::plotlyOutput(
-      outputId = ns("plot_mod")
+    bs4Dash::tabBox(
+      id = ns("tabs_plot_mod"),
+      width = 12,
+      shiny::tabPanel(
+        title = "rank_regression",
+        r_code(
+          'p_weib %>%',
+          '  plot_mod(',
+          '    rr',
+          '    title_trace = "Rank Regression"',
+          '  )'
+        ),
+        plotly::plotlyOutput(
+          outputId = ns("plot_mod_rr")
+        )
+      ),
+      shiny::tabPanel(
+        title = "ml_estimation",
+        r_code(
+          'p_weib %>%',
+          '  plot_mod(',
+          '    mle,',
+          '    title_trace = "ML Estimation',
+          '  )'
+        ),
+        plotly::plotlyOutput(
+          outputId = ns("plot_mod_mle")
+        )
+      )
     )
   )
 }
@@ -35,20 +51,28 @@ gs_plot_mod_server <- function(id,
 
       ns <- session$ns
 
-      plot_mod_r <- shiny::reactive({
+      plot_mod_rr_r <- shiny::reactive({
         plot_prob_r() %>%
           plot_mod(
             rank_regression_r(),
             title_trace = "Rank Regression"
-          ) %>%
+          )
+      })
+
+      plot_mod_mle_r <- shiny::reactive({
+        plot_prob_r() %>%
           plot_mod(
             ml_estimation_r(),
             title_trace = "ML Estimation"
           )
       })
 
-      output$plot_mod <- plotly::renderPlotly({
-        plot_mod_r()
+      output$plot_mod_rr <- plotly::renderPlotly({
+        plot_mod_rr_r()
+      })
+
+      output$plot_mod_mle <- plotly::renderPlotly({
+        plot_mod_mle_r()
       })
     }
   )
